c07f849cc067dd8817697cc7eed670abc681cf2f
JoeWoo <wujian_hit@qq.com>
2013-03-24 19:24:48 +0800

    Finish user edit, update, index,and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: :destroy
+
   def new
     @user = User.new
   end
@@ -13,10 +17,53 @@ class UsersController < ApplicationController
       sign_in @user
       flash[:success]= "Welcome to the Sample App!"
       redirect_to @user
-      #handle a successful save
     else
       render 'new'
     end
   end
 
+  def destroy
+    if User.find(params[:id])!=current_user
+      User.find(params[:id]).destroy
+      flash[:success] = "User destroyed."
+      redirect_to users_path
+    else
+      redirect_to users_path
+      flash[:notice] = "Can`t delete yourself (admin)"
+    end
+  end
+
+  def edit
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please sign in." unless signed_in?
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
 end
---
+++ lib/tasks/sample_data.rake
@@ -0,0 +1,22 @@
+namespace :db  do
+  desc "Fill database with sample data"
+  task populate: :environment do
+    admin = User.create!(
+      name: "root User",
+      email: "root@126.com",
+      password: "foobar",
+    password_confirmation: "foobar")
+    admin.toggle!(:admin)
+
+    99.times do |n|
+      name = Faker::Name.name
+      email = "example-#{n+1}@126.com"
+      password = "password"
+      User.create!(
+        name: name,
+        email: email,
+        password: password,
+      password_confirmation: password)
+    end
+  end
+end
